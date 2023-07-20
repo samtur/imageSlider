6aa006fe9f02f328e7d9191514a4bdd9bb29a75f
samtur <108389765+samtur@users.noreply.github.com>
2023-07-20 11:26:04 +0100

    Updated image carousel with selector icons

--- src/carouselFunction.js
+++ src/carouselFunction.js
@@ -1,6 +1,5 @@
 const carouselFunction = () => {
   const buttons = document.querySelectorAll("[data-carousel-button]");
-  console.log(buttons);
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       const offset = button.dataset.carouselButton === "next" ? 1 : -1;
@@ -8,11 +7,19 @@ const carouselFunction = () => {
         .closest("[data-carousel]")
         .querySelector("[data-slides]");
 
+      const selectors = button
+        .closest("[data-carousel]")
+        .querySelector("[data-selector]");
+
       const activeSlide = slides.querySelector("[data-active]");
+      const activeSelector = selectors.querySelector("[data-active]");
+
       let newIndex = [...slides.children].indexOf(activeSlide) + offset;
       if (newIndex < 0) newIndex = slides.children.length - 1;
       if (newIndex >= slides.children.length) newIndex = 0;
 
+      selectors.children[newIndex].dataset.active = true;
+      delete activeSelector.dataset.active;
       slides.children[newIndex].dataset.active = true;
       delete activeSlide.dataset.active;
     });
